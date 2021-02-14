1114b28dd3a3d8ac4188035e83a825493fed9497
cdevito2 <cdevito2@uwo.ca>
2021-02-14 13:46:02 -0700

    minor fix starting L3 CLI's

--- cmdcodes.h
+++ cmdcodes.h
@@ -20,8 +20,15 @@
 #define __CMDCODES__
 
 #define CMDCODE_SHOW_NW_TOPOLOGY     1
+#define CMDCODE_PING                 2
 #define CMDCODE_RUN_ARP              4
 #define CMDCODE_SHOW_NODE_ARP_TABLE  3
+
+#define CMDCODE_INTF_CONFIG_L2_MODE 5   /*config node <node-name> interface <intf-name> l2mode <access|trunk>*/
+#define CMDCODE_INTF_CONFIG_IP_ADDR 6   /*config node <node-name> interface <intf-name> ip-address <ip-address> <mask>*/
+#define CMDCODE_INTF_CONFIG_VLAN    7   /*config node <node-name> interface <intf-name> vlan <vlan-id>*/
 #define CMDCODE_SHOW_NODE_MAC_TABLE  8
 
+#define CMDCODE_SHOW_NODE_RT_TABLE  9   /*show node <node-name> rt*/
+#define CMDCODE_CONF_NODE_L3ROUTE   10  /*config node <node-name> route <ip-address> <mask> [<gw-ip> <oif>]*/
 #endif
